2f94832c3d0d7c9ed4d1da814b79964392932062
Axel Calås <axel.calas.semst@gmail.com>
2024-04-03 14:00:50 +0200

    Somehow pages didn't hang along

---
+++ Pages/Webstore/OrderConfirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webshop.Pages.Webstore
+{
+    public class OrderConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
