114bc75e360c28ff97c0d500c523744e8e5f02c2
McFlink <mikaelcflink@gmail.com>
2024-04-08 09:53:17 +0200

    Some styling in index-files

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,5 +14,34 @@ namespace Webshop.Pages
         {
             this.database = database;
         }
+
+        public List<Product> Products { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Products = await GetRandomProductAsync(3);
+        }
+
+        public async Task<List<Product>> GetRandomProductAsync(int count)
+        {
+            int totalProducts = await database.Products.CountAsync();
+
+            List<Product> randomProducts = new List<Product>();
+
+            Random random = new Random();
+            while (randomProducts.Count < count)
+            {
+                int offset = random.Next(0, totalProducts);
+
+                var product = await database.Products.Skip(offset).FirstOrDefaultAsync();
+
+                if (product != null && !randomProducts.Contains(product))
+                {
+                    randomProducts.Add(product);
+                }
+            }
+
+            return randomProducts;
+        }
     }
 }
