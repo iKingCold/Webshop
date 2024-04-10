b101ba541a38481dcfcf013aa8d2ba0581c4cfd1
McFlink <mikaelcflink@gmail.com>
2024-04-10 09:20:02 +0200

    Added API

--- Controllers/APIController.cs
+++ Controllers/APIController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
@@ -9,6 +10,7 @@ namespace Webshop.Controllers
 {
     [Route("/api")]
     [ApiController]
+    [AllowAnonymous]
     public class APIController : ControllerBase
     {
         private readonly AppDbContext database;
@@ -19,9 +21,35 @@ namespace Webshop.Controllers
         }
 
         [HttpGet]
-        public List<Product> Products()
+        public List<Product> Products(string? productName, string? category, int page = 1)
         {
-            return database.Products.ToList();
+            List<Product> products = database.Products.Include(p => p.Category).ToList();
+
+            // Filter on name
+            if (productName != null)
+            {
+                products = products.Where(p => p.Name.ToLower().Contains(productName)).ToList();
+            }
+
+            // Filter on category
+            if (category != null)
+            {
+                products = products.Where(p => p.Category.Name.ToLower() == category).ToList();
+            }
+
+            // Pagination
+            products = products.Skip((page - 1) * 10).Take(10).ToList();
+
+
+            // Get image absolute url
+            var absoluteUrl = $"{Request.Scheme}://{Request.Host}";
+
+            foreach (var product in products)
+            {
+                product.Image = $"{absoluteUrl}/Images/{product.Image}";
+            }
+
+            return products;
         }
         
     }
